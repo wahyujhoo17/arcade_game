0dfe64d9b51623351e368f8be49843fb2e165955
diianekawti <diianekawti@gmail.com>
2026-03-11 15:58:49 +0700

    fix: update server port configuration to use environment variable

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ function getLocalIP() {
 
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = '0.0.0.0'
-const port = 3000
+const port = parseInt(process.env.PORT || '3000', 10)
 
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
